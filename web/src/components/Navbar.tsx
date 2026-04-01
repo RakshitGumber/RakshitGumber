d82d0b45c99c8fdc36d820985a6dfee36716c0c5
RakshitGumber <gumberrakshit@gmail.com>
2026-04-01 20:52:16 +0530

    added power button

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -3,19 +3,19 @@ import { PowerButton } from "./PowerButton";
 
 export const Navbar = () => {
   return (
-    <div className="w-full flex items-center justify-end border-b-3 border-border">
+    <div className="w-full flex items-center justify-end border-b-2 border-border fixed h-14">
       <ul className="flex h-full items-center">
-        <li className="px-4 min-w-24 h-full flex items-center justify-center border-l-3 border-border">
+        <li className="px-4 min-w-24 h-full flex items-center justify-center border-l-2 border-border">
           <Link to="/">Work</Link>
         </li>
-        <li className="px-4 min-w-24 h-full flex items-center justify-center border-l-3 border-border">
+        <li className="px-4 min-w-24 h-full flex items-center justify-center border-l-2 border-border">
           <Link to="/blog">Blogs</Link>
         </li>
-        <li className="px-4 min-w-24 h-full flex items-center justify-center border-l-3 border-border">
+        <li className="px-4 min-w-24 h-full flex items-center justify-center border-l-2 border-border">
           <Link to="/about">Projects</Link>
         </li>
       </ul>
-      <div>
+      <div className="flex h-full items-center">
         <PowerButton />
       </div>
     </div>
--- web/src/components/PowerButton.tsx
+++ web/src/components/PowerButton.tsx
@@ -1,9 +1,30 @@
 import { Icon } from "@iconify/react";
+import { useState } from "react";
 
 export const PowerButton = () => {
+  const [showActions, setShowActions] = useState(false);
   return (
-    <button className="px-3 py-4 text-3xl border-l-3 border-border cursor-pointer">
-      <Icon icon="fa6-solid:otter"></Icon>
-    </button>
+    <>
+      <button
+        onClick={() => setShowActions(!showActions)}
+        className="flex items-center justify-center w-16 h-full text-3xl border-l-2 border-border cursor-pointer"
+      >
+        {showActions ? (
+          <Icon icon="fa6-solid:x" />
+        ) : (
+          <Icon icon="fa6-solid:otter" />
+        )}
+      </button>
+      {showActions && (
+        <div className="absolute top-14 right-0 flex flex-col items-center justify-center w-16 border-l-2 border-border">
+          <button className="border-b-2 flex items-center justify-center text-3xl border-border w-full h-12">
+            <Icon icon="fa6-solid:sun"></Icon>
+          </button>
+          <button className="border-b-2 flex items-center justify-center text-3xl border-border w-full h-12">
+            <Icon icon="fa6-solid:file-arrow-down"></Icon>
+          </button>
+        </div>
+      )}
+    </>
   );
 };
