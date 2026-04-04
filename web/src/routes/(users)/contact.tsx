52704b3276525ee6f96599b0802a045927612083
RakshitGumber <gumberrakshit@gmail.com>
2026-04-04 22:48:15 +0530

    migrated to astro

--- web/src/routes/(users)/contact.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/(users)/contact")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div></div>;
-}
--- web/src/types/virtual-blog-content.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "virtual:blog-content" {
-  export const blogEntries: Array<{
-    filename: string;
-    frontmatter: Record<string, unknown>;
-    content: string;
-  }>;
-}
